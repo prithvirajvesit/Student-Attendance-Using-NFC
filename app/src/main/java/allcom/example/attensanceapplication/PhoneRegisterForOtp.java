fe2b24943d4b3cbda6daf9e8f1ca0392170d2eb5
prithvirajvesit <2019prithviraj.chavan@ves.ac.in>
2020-12-01 18:53:03 +0530

    File added

---
+++ app/src/main/java/allcom/example/attensanceapplication/PhoneRegisterForOtp.java
@@ -0,0 +1,13 @@
+package allcom.example.attensanceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneRegisterForOtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_register_for_otp);
+    }
+}
